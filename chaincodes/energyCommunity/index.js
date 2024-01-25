4d5963a55389e31016a99fad4a003a75921db752
Alessandro Bigiotti <alessandro.bigiotti@gmail.com>
2024-01-25 15:50:56 +0000

    Live Share
    Co-authored-by: irenedomenicale <irenedomenicale@users.noreply.github.com>

---
+++ chaincodes/energyCommunity/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const EnergyCommunityContract = require('./lib/EnergyCommunityContract.js')
+module.exports.EnergyCommunityContract = EnergyCommunityContract
+module.exports.contracts = [EnergyCommunityContract]
